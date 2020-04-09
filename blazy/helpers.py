117fe5691956a460ea29d3e7ed4a0786c505162b
Oscar <oscarbranson@gmail.com>
2020-04-09 13:23:03 +0100

    few convenience functions for database listing

--- blazy/helpers.py
+++ blazy/helpers.py
@@ -1,3 +1,8 @@
+import os
+from glob import glob
+import pandas as pd
+import pkg_resources as pkgrs
+
 def issubset(target, reference):
     """
     Returns True is all elements of target are present in reference.
@@ -5,3 +10,31 @@ def issubset(target, reference):
     Both inputs must be iterable.
     """
     return set(target).issubset(set(reference))
+
+def load_reference_data(file=None):
+    if file is None:
+        file = pkgrs.resource_filename('blazy', os.path.join('resources', 'test_data', 'SolutionKs.csv'))
+    
+    return pd.read_csv(file, comment='#')
+
+def list_databases(show_headers=True):
+    dbases = glob(pkgrs.resource_filename('blazy', os.path.join('resources', 'database', '*.dat')))
+
+    for d in sorted(dbases):
+        if show_headers:
+            print(get_database_header(d))
+        else:
+            print(os.path.basename(d))
+        print()
+
+def get_database_header(d):
+    out = [os.path.basename(d)]
+    with open(d, 'r', errors='replace') as f:
+        header = True
+        while header:
+            line = f.readline()
+            if line.startswith('#'):
+                out.append(f'   {line[1:].lstrip().rstrip()}')
+            else:
+                header = False
+    return '\n'.join(out)
